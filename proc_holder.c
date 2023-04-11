e2bf3e9946bdde6d0755745c8fa90c608fcdfc92
spikechris <christian.d.manuel@gmail.com>
2023-04-11 12:27:19 -0400

    added proc_holder after Mana's office Hours

---
+++ proc_holder.c
@@ -0,0 +1,12 @@
+// proc_holder.c
+// 3 functions that Mana said
+#include "proc_holder.h"
+
+// 1 funct for initializing
+void proc_initialize()
+{
+
+}
+// 1 for registering procedure
+
+// 1 for returning code for all procedure
---
+++ proc_holder.h
@@ -0,0 +1,8 @@
+#ifndef _PROC_HOLDER_H
+#define _PROC_HOLDER_H
+
+extern void proc_initialize()
+
+extern 
+
+#endif
